2fd772c905b615a7aa06f4a977b82aeb3e7ffde5
Jason Dalton <jason.dalton@azimuth1.com>
2013-07-08 15:40:31 -0400

    Edited UI input descriptions

--- MihptBundler/ui.R
+++ MihptBundler/ui.R
@@ -4,11 +4,11 @@ shinyUI(pageWithSidebar(
   headerPanel("Labrador - Get MAX response out of MiHPT logs"),
   
   sidebarPanel(
-    fileInput('files', "Choose MiHPT data file (.mhp)", multiple=TRUE),
+    fileInput('files', "Choose your MiHPT data files (.mhp)", multiple=TRUE),
     
     tags$hr(),
     
-    numericInput('toplines','Lines to skip at the top of each file',0,min=0),
+    numericInput('toplines','how many lines to skip at the top of each file',0,min=0),
     checkboxInput('header', 'Is there a Header row', TRUE),
     
     textInput('nas','How are null values coded?',"n/a"),
